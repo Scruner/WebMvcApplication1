b1a926a1e32430eba107aaa86bf9939ce12703d1
Scruner <aoveden1977@mail.ru>
2020-11-30 19:39:52 +0300

    Refactoring final префайнал

--- src/main/java/ru/vdv/jm/security/LoginSuccessHandler.java
+++ src/main/java/ru/vdv/jm/security/LoginSuccessHandler.java
@@ -17,12 +17,12 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
                                         HttpServletResponse httpServletResponse,
-                                        Authentication authentication) throws IOException, ServletException {
+                                        Authentication authentication) throws IOException {
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
-        if (roles.contains("USER")) {
-            httpServletResponse.sendRedirect("/user");
-        } else if (roles.contains("ADMIN")) {
+        if (roles.contains("ADMIN")) {
             httpServletResponse.sendRedirect("/admin");
+        } else if (roles.contains("USER")) {
+            httpServletResponse.sendRedirect("/user");
         } else {
             httpServletResponse.sendRedirect("/");
         }
